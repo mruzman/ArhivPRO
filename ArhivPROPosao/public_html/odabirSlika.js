b164116cf2da4124291b53e421bb429372b4cced
mruzman <mruzman@foi.hr>
2017-02-23 21:45:45 +0100

    Dodavanje preliminarnih stvari
    Js. način kako dalje u js. zapisano u commu

--- ArhivPROPosao/public_html/odabirSlika.js
+++ ArhivPROPosao/public_html/odabirSlika.js
@@ -1,16 +1,8 @@
-var picNum=1;
-prikazi(picNum);
+$(document).ready(function (){
+    console.log("ready");
+   // for(int i=0;i<onogkajdobimnaserveru.length;i++){
+   // $("#image-slider").append("<li>"+ onokajdobimnaserveru[i]+"</li>");
+   // }
+});
 
-
-function prikazi(n){
-    var i=0;
-    var x=document.getElementsByClassName("preuzeto");
-    for(i=0;i<x.length;i++){
-        x[i].style.display = "none";
-    }
-    picNum++;
-    if(picNum>x.length) {picNum=1;}
-    x[picNum-1].style.display ="block";
-    setTimeout(prikazi, 2000);
-}
 
---
+++ Sortiranje_zadatak/index.php
@@ -0,0 +1,11 @@
+<?php
+?>
+
+<html>
+    <head>
+        <title>Naslov</title>
+    </head>
+    <body>
+        <p>Test</p>
+    </body>
+</html>
